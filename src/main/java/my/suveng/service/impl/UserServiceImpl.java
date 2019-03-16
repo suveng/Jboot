378750754d406c430f720180e6475d6d1f008f1d
suveng <suveng@163.com>
2019-03-16 13:43:02 +0800

    user-cache data 用户模块-缓存

--- src/main/java/my/suveng/service/impl/UserServiceImpl.java
+++ src/main/java/my/suveng/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package my.suveng.service.impl;
 
 import com.jfinal.plugin.activerecord.Page;
 import io.jboot.aop.annotation.Bean;
+import io.jboot.core.cache.annotation.CacheEvict;
+import io.jboot.core.cache.annotation.Cacheable;
 import my.suveng.service.UserService;
 import my.suveng.model.User;
 import io.jboot.service.JbootServiceBase;
@@ -12,7 +14,21 @@ import javax.inject.Singleton;
 @Singleton
 public class UserServiceImpl extends JbootServiceBase<User> implements UserService {
     @Override
+    @Cacheable(name = "myCache",key = "page:#(page)-#(size)")
     public Page<User> paginate(int page, int size) {
         return DAO.paginate(page, size);
+    }
+
+    /**
+     * 保存到数据库
+     *
+     * @param model
+     *
+     * @return
+     */
+    @Override
+    @CacheEvict(name = "myCache",key = "*")
+    public boolean save(User model) {
+        return super.save(model);
     }
 }
