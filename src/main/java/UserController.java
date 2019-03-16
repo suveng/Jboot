2b690f6a82771569c9c5cadb074de0614f87e805
suveng <suveng@163.com>
2019-03-16 13:25:32 +0800

    user-add 用户模块-新增操作

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -2,7 +2,6 @@ import com.jfinal.plugin.activerecord.Page;
 import io.jboot.web.controller.JbootController;
 import io.jboot.web.controller.annotation.RequestMapping;
 import my.suveng.model.User;
-import my.suveng.service.UserService;
 import my.suveng.service.impl.UserServiceImpl;
 
 import javax.inject.Inject;
@@ -24,6 +23,23 @@ public class UserController extends JbootController {
         Page<User> userPage = userService.paginate(page, 10);
         setAttr("pageData", userPage);
         render("/user.html");
+    }
+
+    public void add() {
+        render("/add.html");
+    }
+
+    public void doSave() {
+        String username = getPara("username");
+        String password = getPara("password");
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        user.save();
+
+        redirect("/user");
     }
 
 }
