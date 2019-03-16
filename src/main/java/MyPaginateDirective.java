0700ff51fd5f1a19a5aa33ccd4dc96d0ba9cf7bc
suveng <suveng@163.com>
2019-03-16 13:20:30 +0800

    user-Paging query 用户模块-分页查询

---
+++ src/main/java/MyPaginateDirective.java
@@ -0,0 +1,17 @@
+import io.jboot.web.directive.JbootPaginateDirective;
+import io.jboot.web.directive.annotation.JFinalDirective;
+
+/**
+ * @author suwenguang
+ * suveng@163.com
+ * since 2019/3/16
+ * description:
+ **/
+@JFinalDirective("myPaginate")
+public class MyPaginateDirective extends JbootPaginateDirective {
+
+    @Override
+    protected String getPageAttrName() {
+        return "pageData"; //这个值要和Controller里setAttr的第一个参数值相同
+    }
+}
